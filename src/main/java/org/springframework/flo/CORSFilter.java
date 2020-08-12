f142dbe6ea4d24b42d6610e6770688583f460aa9
usmananwar <usmananwar@outlook.com>
2020-08-12 11:46:02 +0900

    Update CORSFilter.java

--- src/main/java/org/springframework/flo/CORSFilter.java
+++ src/main/java/org/springframework/flo/CORSFilter.java
@@ -4,15 +4,25 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebFilter(urlPatterns = "/**")
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		System.out.println("########## Initiating Custom filter ##########");
+	}
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
