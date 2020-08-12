5caec3544c01a8666e9d53d4ee93ba038c5c6738
usmananwar <usmananwar@outlook.com>
2020-08-12 11:38:18 +0900

    Update CORSFilter.java

--- src/main/java/org/springframework/flo/CORSFilter.java
+++ src/main/java/org/springframework/flo/CORSFilter.java
@@ -2,32 +2,34 @@ package org.springframework.flo;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CORSFilter extends OncePerRequestFilter {
+@WebFilter(urlPatterns = "/**")
+public class CORSFilter implements Filter {
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest req, HttpServletResponse httpServletResponse,
-			FilterChain filterChain) throws ServletException, IOException {
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
 
-		System.out.println("Request URI is: " + req.getRequestURI());
+		HttpServletResponse response = (HttpServletResponse) res;
+		HttpServletRequest request = (HttpServletRequest) req;
 
-		httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-		httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
-		httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
-		httpServletResponse.setHeader("Access-Control-Allow-Headers", "*");
+		System.out.println("Request URI is: " + request.getRequestURI());
 
-		filterChain.doFilter(req, httpServletResponse);
-		System.out.println("Response Status Code is: " + httpServletResponse.getStatus());
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "*");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "*");
+		chain.doFilter(request, response);
+
+		System.out.println("Response Status Code is: " + response.getStatus());
 	}
+
 }
