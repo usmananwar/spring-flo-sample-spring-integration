c8feef4339804eb17d38441b37b8881f26cb0f97
usmananwar <usmananwar@outlook.com>
2020-08-12 10:06:22 +0900

    Update Application.java

--- src/main/java/org/springframework/flo/Application.java
+++ src/main/java/org/springframework/flo/Application.java
@@ -17,10 +17,6 @@ package org.springframework.flo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 
@@ -32,15 +28,5 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/").allowedOrigins("http://localhost:9000");
-			}
-		};
 	}
 }
